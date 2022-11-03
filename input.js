d3497436ef8285fa2379acc5c12812789cb253fc
Agu <gkn060702@gmail.com>
2022-11-04 01:11:31 +0800

    Added input.js

---
+++ input.js
@@ -0,0 +1,24 @@
+export class InputHandle {
+    constructor(){
+        this.keys = [];
+        window.addEventListener('keydown', e =>{
+            if ((e.key === " " ||
+                e.key === "ArrowUp" ||
+                e.key === "ArrowLeft" ||
+                e.key === "ArrowRight"
+                ) && this.keys.indexOf(e.key) === -1){
+                this.keys.push(e.key);
+            }
+        });
+
+        window.addEventListener('keyup', e => {
+            if((e.key === " " ||
+                e.key === "ArrowUp" ||
+                e.key === "ArrowLeft" ||
+                e.key === "ArrowRight"
+                )){
+                this.keys.splice(this.keys.indexOf(e.key), 1);
+            }
+        });
+    }
+}
