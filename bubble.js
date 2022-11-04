868346b46dfa15a9cb82bc7014cbc50ff81d15cf
Agu <gkn060702@gmail.com>
2022-11-05 00:27:58 +0800

    Update bubble.js

--- bubble.js
+++ bubble.js
@@ -1,22 +1,53 @@
-export class Bubble{
+class Bubble{
     constructor(enemy, game){
         this.game = game;
         this.enemy = enemy;
-        this.content = enemy.hit;
-        this.fontsize = 30;
-        this.fontFamily = 'Helvetica';
+        this.content = enemy.hit.length;
+        this.bubblewidth = 50;
+        this.bubbleheight = 50;
+        this.locx = this.enemy.x;
     }
 
     update(){
-        this.content = this.enemy.hit;
+        this.content = this.enemy.hit.length;
+        this.locx = this.enemy.x;
     }
 
     draw(context){
-        context.font = this.fontsize + 'px ' + this.fontFamily;
-        context.textAlign = 'left';
-        context.fillStyle = this.game.fontColor;
+        //context.drawImage(this.bubbleimage, this.locx, this.enemy.y - this.bubbleheight - 10);
+        var startx = this.locx + (this.enemy.width / 2);
+        for(var i=0; i<this.enemy.hit.length; i++){
+            context.drawImage(this.bubbleimage, startx - ((this.enemy.hit.length - i) * this.bubblewidth / 2), this.enemy.y - this.bubbleheight - 10, this.bubblewidth, this.bubbleheight);
+        }
+    }
+}
 
-        //score
-        context.fillText(this.content, this.enemy.x, this.enemy.y - 10);
+export class BBubble extends Bubble{
+    constructor(enemy, game){
+        super(enemy, game);
+        this.bubbleimage = document.getElementById('bbubble');
+    }
+
+    update(){
+        super.update();
+    }
+
+    draw(context){
+        super.draw(context);
+    }
+}
+
+export class OBubble extends Bubble{
+    constructor(enemy, game){
+        super(enemy, game);
+        this.bubbleimage = document.getElementById('obubble');
+    }
+
+    update(){
+        super.update();
+    }
+
+    draw(context){
+        super.draw(context);
     }
 }
