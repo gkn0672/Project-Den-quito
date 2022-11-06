f139178c85f6b9b8013178a9d875656008c2204b
Agu <gkn060702@gmail.com>
2022-11-06 23:30:34 +0800

    Update bubble.js

--- bubble.js
+++ bubble.js
@@ -14,7 +14,6 @@ class Bubble{
     }
 
     draw(context){
-        //context.drawImage(this.bubbleimage, this.locx, this.enemy.y - this.bubbleheight - 10);
         var startx = this.locx + (this.enemy.width / 2);
         for(var i=0; i<this.enemy.hit.length; i++){
             context.drawImage(this.bubbleimage, startx - ((this.enemy.hit.length - i) * this.bubblewidth / 2), this.enemy.y - this.bubbleheight - 10, this.bubblewidth, this.bubbleheight);
@@ -41,6 +40,21 @@ export class OBubble extends Bubble{
     constructor(enemy, game){
         super(enemy, game);
         this.bubbleimage = document.getElementById('obubble');
+    }
+
+    update(){
+        super.update();
+    }
+
+    draw(context){
+        super.draw(context);
+    }
+}
+
+export class LBubble extends Bubble{
+    constructor(enemy, game){
+        super(enemy, game);
+        this.bubbleimage = document.getElementById('lbubble');
     }
 
     update(){
